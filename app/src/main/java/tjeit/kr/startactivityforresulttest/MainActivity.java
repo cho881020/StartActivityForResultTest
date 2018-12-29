7b69f79854ad0d5d8ee2a24b0e4669e65d510c74
Daeki <kimdaeki@gmail.com>
2018-12-29 16:02:21 +0900

    크롭 이미지 기능 작성 (미완성)

--- app/src/main/java/tjeit/kr/startactivityforresulttest/MainActivity.java
+++ app/src/main/java/tjeit/kr/startactivityforresulttest/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends BaseActivity {
     final static int REQUEST_FOR_USER_NAME = 1000;
     final static int REQUEST_FOR_USER_BIRTHDAY = 1001;
     final static int REQUEST_FOR_PICTURE_GALLERY = 1002;
+    final static int REQUEST_FOR_PICTURE_CROP = 1003;
 
     private android.widget.TextView nameTxt;
     private android.widget.Button nameInputBtn;
@@ -56,13 +57,12 @@ public class MainActivity extends BaseActivity {
             if (resultCode == RESULT_OK) {
                 Uri selectedImageUri = data.getData();
 //                String imagePath = selectedImageUri.getPath();
-
                 Glide.with(mContext).load(selectedImageUri).into(profileImg);
 
+//                cropImage(selectedImageUri);
 
             }
         }
-
     }
 
     @Override
@@ -107,6 +107,25 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+    }
+
+    private void cropImage(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", true);
+
+//        잘라낸 파일의 크기
+        intent.putExtra("outputX", 320);
+        intent.putExtra("outputY", 320);
+
+//        잘라내는 비율
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+
+        intent.putExtra("return-data", true);
+
+        startActivityForResult(intent, REQUEST_FOR_PICTURE_CROP);
 
     }
 
